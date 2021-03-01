73e11e1ec1ab97274328ea400a160de7d000d7f0
Kyle Conkright <kyleconkright@gmail.com>
2021-02-28 21:14:38 -0800

    client update

--- api/src/app.ts
+++ api/src/app.ts
@@ -55,7 +55,7 @@ class App {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(cors({
-      origin: "http://localhost:5000",
+      origin: process.env.CLIENT_URL,
       credentials: true
     }));
 
