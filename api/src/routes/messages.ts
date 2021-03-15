4aeddb23e10eaa42ed1206c55fcba60969081b7a
Kyle Conkright <kyleconkright@gmail.com>
2021-03-14 22:31:17 -0700

    twilio

--- api/src/routes/messages.ts
+++ api/src/routes/messages.ts
@@ -1,20 +1,31 @@
 import { Request, Response } from 'express';
+import twilio = require('twilio');
 import { TwilioClient } from '../controllers/twilio/twilio';
 
 export class MessagesRoutes {
 
   public routes(app) {
 
-    const twilio = new TwilioClient();
+    const twilioClient = new TwilioClient();
 
     app.post('/messages/update', async (req: Request, res: Response) => {
       const { data } = await req.body;
       try {
-        const twilioResponse = await twilio.sendAlert(data);
+        const twilioResponse = await twilioClient.sendAlert(data);
         res.json(twilioResponse);
       } catch(error) {
         console.error(error);
         res.json(error);
+      }
+    })
+    
+    app.post('/messages/handle', twilio.webhook({validate: false}), async (req, res) => {
+      try {
+        var from = req.body.From;
+        var smsRequest = req.body.Body;
+        res.json({from, smsRequest});
+      } catch(error) {
+        console.error(error);
       }
     })
   }
