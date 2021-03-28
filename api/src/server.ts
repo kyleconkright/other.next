b891e0bb38ab5a74502773d69f117f8415285736
Kyle Conkright <kyleconkright@Kyles-MacBook-Air.local>
2021-03-27 17:39:53 -0700

    prepare for socket

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,5 @@
 import app from './app';
 
-<<<<<<< HEAD
-app.listen(5001, () => {
-  console.log(`Server Running... ${process.env.API_URL}`);
-=======
 const http = require("http");
 const socketIo = require("socket.io");
 const server = http.createServer(app);
@@ -17,5 +13,4 @@ export const io = socketIo(server, {
 
 server.listen(5001, () => {
   console.log(`Server Running... ${process.env.CLIENT_URL}`);
->>>>>>> @{-1}
 })
