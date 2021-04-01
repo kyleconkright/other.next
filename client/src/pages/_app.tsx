aed14fd4e28c2aee7946c21bab6c34807de1b60f
Kyle Conkright <kyleconkright@Kyles-MacBook-Air.local>
2021-04-01 11:05:46 -0700

    ga

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,10 +2,13 @@ import './../styles/stylesheet.scss';
 
 import { Provider } from "react-redux";
 import store from '../store';
-import React from 'react';
+import React, { useEffect } from 'react';
 import UserContextProvider from './../contexts/user.context';
+import { useRouter } from 'next/router';
+import * as gtag from "./../../utils/gtag";
 
 export default function OtherApp({ Component, pageProps }) {
+  const router = useRouter();
 
   //   async function getInitialProps({Component, ctx}) {
   //     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
@@ -13,6 +16,16 @@ export default function OtherApp({ Component, pageProps }) {
   //     //Anything returned here can be access by the client
   //     return {pageProps: pageProps};
   // }
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <Provider store={store}>
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from "./../../utils/gtag";
 
 class OtherDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,7 +10,24 @@ class OtherDocument extends Document {
   render() {
     return (
       <Html>
-       <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6637836/7151992/css/fonts.css" />
+        <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6637836/7151992/css/fonts.css" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `
+          }}
+        />
         <Head />
         <body>
           <Main />
